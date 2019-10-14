52a64d6936e9026eb9a3ada4572562a0c2560c4f
Nicholas Johnson <sylvyrfysh@gmail.com>
2019-10-14 14:32:12 -0500

    Fix python path finding

--- python/CompProg1920/npj/__init__.py
+++ python/CompProg1920/npj/__init__.py
@@ -1,6 +1,13 @@
 from pathlib import Path
 
-resource_path = Path('../../resources')
+resource_path = Path(Path().absolute().parent.parent).absolute()
+
+while resource_path.name != 'CompProg1920':
+    resource_path = resource_path.parent
+
+resource_path = resource_path.joinpath(Path('resources'))
+
+print(resource_path)
 
 if not resource_path.exists():
     raise Exception('Resources do not appear to have been extracted! Run `resources.bat` in the root of this project '
